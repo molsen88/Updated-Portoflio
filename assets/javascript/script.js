2bc8454b431a419232dcfee1a85585f902977257
Michael Olsen <michaelolsen@Michaels-MacBook-Air.local>
2019-04-08 18:23:25 -0600

    getting button to work

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,15 @@
+submit();
+function submit() {
+    var firstName = document.getElementById( "first_name" ).value;
+    console.log( firstName );
+    var lastName = document.getElementById( "last_name" ).value;
+    console.log( lastName );
+    var email = document.getElementById( "email" ).value;
+    console.log( email );
+    var message = document.getElementById( "icon_prefix2" ).value;
+    console.log( message );
 
+}
 document.addEventListener( 'DOMContentLoaded', function () {
     var elems = document.querySelectorAll( '.collapsible' );
     var instances = M.Collapsible.init( elems, instances );
