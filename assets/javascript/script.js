590d19519d7f5092497c250d7e44f0d2d1aeee92
Michael Olsen <michaelolsen@Michaels-MacBook-Air.local>
2019-04-08 23:01:49 -0600

    collapsible

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,11 +10,13 @@
 //     console.log( message );
 
 // }
+
 document.addEventListener( 'DOMContentLoaded', function () {
     var elems = document.querySelectorAll( '.collapsible' );
     var instances = M.Collapsible.init( elems, instances );
-    // console.log( instances )
 } );
+
+
 document.addEventListener( 'DOMContentLoaded', function () {
     var elems = document.querySelectorAll( '.carousel' );
     var instances = M.Carousel.init( elems, instances );
